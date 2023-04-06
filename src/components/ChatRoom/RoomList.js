ca237bcd72860c07dc3a20b04c37590715758274
Pham Hieu <phamngochieu2001gl@gmail.com>
2023-04-06 14:01:21 +0700

    fix error

--- src/components/ChatRoom/RoomList.js
+++ src/components/ChatRoom/RoomList.js
@@ -40,7 +40,7 @@ export default function RoomList() {
     <Collapse ghost defaultActiveKey={["1"]}>
       <PanelStyled header="Danh sách các phòng" key="1">
         {
-            roomList.map((room, index) =>  <LinkStyled key={room.id} onClick={() => setSelectedRoomId(room.id)} >{room.name}</LinkStyled>)
+            roomList.map((room) =>  <LinkStyled key={room.id} onClick={() => setSelectedRoomId(room.id)}>{room.name}</LinkStyled>)
         }
 
         <Button type="text" icon={<PlusSquareOutlined />} className="add-room" onClick={handleAddRoom}>
